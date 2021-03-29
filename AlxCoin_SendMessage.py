fba2a80a4f7b0e6563bb2be7ed9bf09aac8cc757
AlxKorea <alx.morning@gmail.com>
2021-03-30 01:05:06 +0900

    upbit change gopax

--- AlxCoin_SendMessage.py
+++ AlxCoin_SendMessage.py
@@ -1,16 +1,19 @@
 from telegram.ext import Updater
-from upbitpy import Upbitpy
-import time
-import datetime
 import logging
-import os
-import ast
+import datetime
+import base64, hashlib, hmac, json, requests, time, os
 
-INTERVAL_MIN_TIME = 30
 
-MY_MARKETS = ast.literal_eval(os.environ["MY_MARKETS"])
+API_KEY = os.environ["GOPAX_API_KEY"]
+SECRET = os.environ["GOPAX_SECRET"]
+MY_ASSET = os.environ["GOPAX_MY_ASSET"]
+MyAsset = '/trading-pairs/'+ MY_ASSET + '/ticker'
 CHAT_ID = os.environ["CHAT_ID"]
 TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+MY_CHECK_ALERT = os.environ["MY_CHECK_PRICE"]
+
+INTERVAL_MIN_TIME = 1
+
 
 def wait(min) :
     now = datetime.datetime.now()
@@ -18,39 +21,79 @@ def wait(min) :
     remain_second += 60 * (min - (now.minute % min + 1))
     time.sleep(remain_second)
 
+def call(need_auth, method, path, body_json=None, recv_window=None) :
+    method = method.upper()
+    if need_auth :
+        timestamp = str(int(time.time() * 1000))
+        include_querystring = method == 'GET' and path.startswith('/orders?')
+        p = path if include_querystring else path.split('?')[0]
+        msg = 't' + timestamp + method + p
+        msg += (str(recv_window) if recv_window else '') + (json.dumps(body_json) if body_json else '')
+        raw_secret = base64.b64decode(SECRET)
+        raw_signature = hmac.new(raw_secret, str(msg).encode('utf-8'), hashlib.sha512).digest()
+        signature = base64.b64encode(raw_signature)
+        headers = {'api-key' : API_KEY, 'timestamp' : timestamp, 'signature' : signature}
+
+        if recv_window :
+            headers['receive-window'] = str(recv_window)
+
+    else :
+        headers = {}
+
+    req_func = {
+        'GET' : requests.get,
+        'POST' : requests.post,
+        'DELETE' : requests.delete
+    }[method]
+    
+    resp = req_func(
+        url = 'https://api.gopax.co.kr' + path,
+        headers = headers,
+        json = body_json
+    )
+    return {
+        'statusCode': resp.status_code,
+        'body': resp.json(),
+        'header': dict(resp.headers),
+    }
+
+
+
 def main() :
-    upbit = Upbitpy()
+
     updater = Updater(TELEGRAM_BOT_TOKEN)
+    check_alert_use = True
+    regular_interval = 0
 
-    #print(MY_MARKETS)
-
-    while True :
-        ticker = upbit.get_ticker(MY_MARKETS)
-
-        sendMessage = '({}) \n'.format(datetime.datetime.now().strftime('%m/%d %H:%M:%S'))
-        
-        for market_list in ticker :
-
-            market_name = market_list['market']
-
-            if market_list['market'].startswith('KRW') :
-                trade_price = format(float(market_list['trade_price']), ',.2f')
-                closing_price = format(float(market_list['prev_closing_price']), ',.2f')
-            elif market_list['market'].startswith('BTC') :
-                trade_price = format(float(market_list['trade_price']) * 100000000, ',.0f') + ' BTC'
-                closing_price = format(float(market_list['prev_closing_price']) * 100000000, ',.0f') + ' BTC'
-            else :
-                pass
-
-            change_rate = format(float(market_list['signed_change_rate']) * 100, '.2f') + '%'
-            
-            sendMessage += '[{}] {} ({} /{})\n'.format(
-                market_name, trade_price, closing_price, change_rate
-            )
+    def send_message_func() :
+        sendMessage = '({}) '.format(datetime.datetime.now().strftime('%m/%d %H:%M:%S'))
+        sendMessage += '{} 현재가 [ {} ] \n + 매도호가 : {}/ {}/ {} \n + 매수호가 : {}/ {}/ {}\n + 24시간거래량 : {} KRW'.format(
+            MY_ASSET, receiveTickerBody['price'],
+            receiveTickerBody['ask'], receiveTickerBody['askVolume'], format(float(receiveTickerBody['ask'] * receiveTickerBody['askVolume']), ',.2f'),
+            receiveTickerBody['bid'], receiveTickerBody['bidVolume'], format(float(receiveTickerBody['bid'] * receiveTickerBody['bidVolume']), ',.2f'),
+            format(float(receiveTickerBody['quoteVolume']), ',.2f'),
+        )
 
         #print(sendMessage)
         updater.bot.send_message(chat_id = CHAT_ID, text = sendMessage)
 
+    while True :
+        receiveTicker = call(True, 'GET', MyAsset)
+        receiveTickerBody = receiveTicker['body']
+        #print(receiveTickerBody)
+
+        if receiveTickerBody['price'] > MY_CHECK_ALERT and check_alert_use :
+            send_message_func()
+            check_alert_use = False
+
+        if receiveTickerBody['price'] < MY_CHECK_ALERT and not check_alert_use :
+            check_alert_use = True
+
+        if regular_interval >= 2 :
+            send_message_func()
+            regular_interval = 0
+
+        regular_interval += 1
         wait(INTERVAL_MIN_TIME)
 
 
